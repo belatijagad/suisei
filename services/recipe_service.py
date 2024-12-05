ef0f8eb6f471a7eebeaf695a43720ac444dd2e5e
Belati <belatijagadbintangsyuhada@gmail.com>
2024-12-05 17:36:29 +0700

    nyoba tanpa wikidata

--- services/recipe_service.py
+++ services/recipe_service.py
@@ -9,12 +9,12 @@ class RecipeService:
 
   async def search_recipes(self, title: Optional[str], category: Optional[str], ingredients: Optional[List[str]]) -> List[Dict]:
     local_results = await self.kg.search(title, category, ingredients)   
-    for i in range(len(local_results)):
-      result = local_results[i]
-      wikidata_code = result.get('wikicode')
-      if wikidata_code:
-        wikidata_info = self.wikidata.get_info_from_wikidata(wikidata_code)
-        result.update(wikidata_info) 
-        local_results[i] = result
+    # for i in range(len(local_results)):
+    #   result = local_results[i]
+    #   wikidata_code = result.get('wikicode')
+    #   if wikidata_code:
+    #     wikidata_info = self.wikidata.get_info_from_wikidata(wikidata_code)
+    #     result.update(wikidata_info) 
+    #     local_results[i] = result
         
     return local_results
